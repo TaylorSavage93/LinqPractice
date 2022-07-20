44a60400424bae160f35e693bf224b8191d17c59
Taylor Savage <taylorsavage93@gmail.com>
2022-07-20 15:18:37 -0400

    finished exercise 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+var videogames = new List<string>() { "minecraft", "farcry", "sims", "worldofwarcraft" };
+
+
+
+
+IOrderedEnumerable<string> List
+    = videogames.OrderBy(x => x.Length);
+
+
+videogames.OrderBy(x => x.Length).ToList().ForEach(videogames => Console.WriteLine(videogames));  
